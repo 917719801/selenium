393552ae5da8f8b9bdef8f2daf7a627e4aba05c6
le <917719801@qq.com>
2020-10-26 20:19:58 +0800

    添加浏览器关闭功能，和浏览器窗口操作功能

--- src/test/java/Selenium/Selenium_API.java
+++ src/test/java/Selenium/Selenium_API.java
@@ -1,7 +1,10 @@
 package Selenium;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -37,7 +40,25 @@ public class Selenium_API {
         driver.navigate().to(baseUrl1);//先访问搜狗
         driver.navigate().to(baseUrl2);//再访问百度
         driver.navigate().back();//再返回搜狗
+        driver.navigate().forward();//从搜狗页面跳转到百度页面，模拟浏览器前进按钮
+        driver.navigate().refresh();//刷新当前页面
+    }
+    @Test
+    void operateBrowser(){
+        String baseUrl = "https://www.sogou.com/";
+//        Point point = new Point(150,150);
+//        Dimension dimension = new Dimension(500,500);
+//        driver.manage().window().setPosition(point);
+//        driver.manage().window().setSize(dimension);
+//        System.out.println(driver.manage().window().getPosition());
+//        System.out.println(driver.manage().window().getSize());
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
 
+    }
+    @AfterAll
+    public static void afterall(){
+        driver.quit();
     }
 
 }
